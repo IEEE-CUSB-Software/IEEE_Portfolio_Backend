c87b72e315fc1a21877077d52bfff064a632f9ee
Ahmed Fathy <ahmedfathi20044002@gmail.com>
2026-09-18 21:05:34 +0300

    fix: use quay.io for minio images

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -36,7 +36,7 @@ docker-compose exec -T postgres pg_dump -U "$DB_USER" -d "$DB_NAME" > "$DB_BACKU
 # 2. Backup MinIO Files (S3 bucket data)
 echo "Backing up MinIO files..."
 mkdir -p "$BACKUP_DIR/minio_tmp"
-docker run --rm -v $(pwd)/$BACKUP_DIR/minio_tmp:/export --network="$NETWORK_NAME" minio/mc \
+docker run --rm -v $(pwd)/$BACKUP_DIR/minio_tmp:/export --network="$NETWORK_NAME" quay.io/minio/mc \
   /bin/sh -c "mc config host add myminio http://minio:9000 $MINIO_USER $MINIO_PASS; mc mirror myminio/$BUCKET_NAME /export"
 tar -czvf "$MINIO_BACKUP_FILE" -C "$BACKUP_DIR/minio_tmp" .
 rm -rf "$BACKUP_DIR/minio_tmp"
